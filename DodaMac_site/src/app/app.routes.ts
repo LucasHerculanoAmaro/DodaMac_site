c6fa2a6d6182e9728d935e8e3e980393d0e2a35c
Lucas Amaro <lucash.96@hotmail.com>
2024-01-07 23:53:10 -0300

    Criando os componentes: Menu, Rodapé e Conteúdo

--- DodaMac_site/src/app/app.routes.ts
+++ DodaMac_site/src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ConteudoComponent } from './conteudo/conteudo.component';
+import { MenuComponent } from './menu/menu.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', redirectTo:'conteudo', pathMatch:'full'},
+    {path:'conteudo', component: ConteudoComponent},
+    {path:'menu', component: MenuComponent},
+    {path:'rodape', component: RodapeComponent}
+];
